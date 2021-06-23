cc3ddb9f86a48f4f6a536d7af4a92e051d297159
bartzilla <ciprianosanchez@gmail.com>
2021-06-23 21:56:01 +0200

    initial commit

---
+++ src/main/java/com/bartzilla/core/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.bartzilla.core;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final Long id;
+
+    public BaseEntity() {
+        this.id = null;
+    }
+}
---
+++ src/main/java/com/bartzilla/review/ReviewRepository.java
@@ -0,0 +1,7 @@
+package com.bartzilla.review;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+
+}
