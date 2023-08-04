c07c087f1c4737bf5311e56c6d2d01beaf3fac26
bartzilla <ciprianosanchez@gmail.com>
2023-08-04 18:07:44 +0200

    Initial fix for basic auth

--- src/main/java/com/bartzilla/core/WebSecurityConfiguration.java
+++ src/main/java/com/bartzilla/core/WebSecurityConfiguration.java
@@ -1,4 +1,51 @@
 package com.bartzilla.core;
 
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurityConfiguration  {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("mike")
+                .password(encoder().encode("important"))
+                .roles("CAPTAIN", "CREW")
+                .build();
+        UserDetails user2 = User.builder()
+                .username("henrik")
+                .password(encoder().encode("important"))
+                .roles("CREW")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/**").hasAnyRole("CAPTAIN", "CREW")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
