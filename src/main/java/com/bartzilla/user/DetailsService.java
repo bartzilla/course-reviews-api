95f221da2008fe677520ac5574ae687a18e94664
bartzilla <ciprianosanchez@gmail.com>
2023-08-07 22:12:04 +0200

    DetailsService implementation

--- src/main/java/com/bartzilla/user/DetailsService.java
+++ src/main/java/com/bartzilla/user/DetailsService.java
@@ -8,25 +8,25 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DetailsService //implements UserDetailsService
+public class DetailsService implements UserDetailsService
 {
 
-//    @Autowired
-//    UserRepository users;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        final User user = users.findByUsername(username);
-//
-//        if (user == null) {
-//            throw new UsernameNotFoundException(username + " was not found");
-//        }
-//
-//        return new org.springframework.security.core.userdetails.User(
-//                user.getUsername(),
-//                user.getPassword(),
-//                AuthorityUtils.createAuthorityList(user.getRoles())
-//        );
-//    }
+    @Autowired
+    UserRepository users;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final User user = users.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username + " was not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                AuthorityUtils.createAuthorityList(user.getRoles())
+        );
+    }
 
 }
